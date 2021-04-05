1966c4e67048e772785a571f4a25a68a599bf07a
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-05 12:31:45 -0400

    vacations attributes listed

--- src/components/vacations/Vacation.js
+++ src/components/vacations/Vacation.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class Vacation extends Component {
+
   render() {
+    const { location, startDate, endDate, budget, vacation } = this.props;
+    
     return (
       <div>
-        Vacation
+        <h3><Link to={`/vacations/${vacation.id}`} >{ location }</Link></h3>
+        <p>
+          { startDate } - { endDate } <br/><br/>
+          $ { budget }
+        </p>
       </div>
     )
   }
--- src/components/vacations/VacationList.js
+++ src/components/vacations/VacationList.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import Vacation from './Vacation';
+import ActivityList from '../activities/ActivityList';
 
 class VacationList extends Component {
   render() {
+    
+    const vacations = this.props.vacations.vacations.map( (vacation, i) => <Vacation key={vacation.id} location={ vacation.location } startDate={ vacation.start_date } endDate={ vacation.end_date } budget={vacation.budget} vacation={vacation} />)
+   
     return (
       <div>
-        Vacation List 
+        <h2>Vacation Spots:</h2>
+        { vacations }
+        <ActivityList />
       </div>
     )
   }
 }
 
-export default VacationList
+const mapStateToProps = state => {
+  return {
+    vacations: state.vacations,
+    activities: state.activities
+  }
+}
+
+export default connect(mapStateToProps)(VacationList)
