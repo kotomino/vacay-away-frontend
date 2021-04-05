73d7ff8fd0eb1b5e908979d321e7c607b42f29bd
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-05 12:32:15 -0400

    loading and set activities reducer

--- src/reducers/activitiesReducer.js
+++ src/reducers/activitiesReducer.js
@@ -5,6 +5,17 @@ const initialState = {
 
 const activitiesReducer = (state = initialState, action) => {
   switch(action.type) {
+    case "LOADING":
+      return {
+        ...state,
+        loading: true
+      }
+    case "SET_ACTIVITIES":
+      return {
+        ...state,
+        loading: false,
+        activities: action.activities
+      }
     default:
       return state;
   }
