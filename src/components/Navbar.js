c35d51cad2f355a10f7b2ff981ea478490e3be37
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-05 12:31:07 -0400

    navbar content edited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
     return (
       <div>
-        Navbar
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/vacations">Vacations</Link></li>
+          <li><Link to="/vacations/new">Plan a new trip</Link></li>
+        </ul>
       </div>
     )
   }
