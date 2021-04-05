f96ad4d827c8a97933e60df044d3ab79511a7a8e
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-05 12:31:30 -0400

    activity attributes listed

--- src/components/activities/Activity.js
+++ src/components/activities/Activity.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
 
+
 class Activity extends Component {
   render() {
+    const { name, address, cost, notes, mondayOpen, mondayClose, tuesdayOpen, tuesdayClose, wednesdayOpen, wednesdayClose, thursdayOpen, thursdayClose, fridayOpen, fridayClose, saturdayOpen, saturdayClose, sundayOpen, sundayClose } = this.props;
+
     return (
       <div>
-        Activity
+        <h3>{ name }</h3>
+        <p>
+          { address } <br/> <br/>
+          $ { cost } <br/><br/>
+          <u>Notes:</u> <br/>
+          { notes ? notes : "N/A" } <br/><br/>
+          <u>Hours of Operation:</u> <br/>
+          Monday: { mondayOpen } - { mondayClose } <br/>
+          Tuesday: { tuesdayOpen } - { tuesdayClose } <br/>
+          Wednesday: { wednesdayOpen } - { wednesdayClose } <br/>
+          Thursday: { thursdayOpen } - { thursdayClose } <br/>
+          Friday: { fridayOpen } - { fridayClose } <br/>
+          Saturday: { saturdayOpen } - { saturdayClose } <br/>
+          Sunday: { sundayOpen } - { sundayClose } <br/>
+        </p>
       </div>
     )
   }
 }
+
+
 
 export default Activity
--- src/components/activities/ActivityList.js
+++ src/components/activities/ActivityList.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import Activity from './Activity';
 
 class ActivityList extends Component {
+  
   render() {
+    
+    const activities = this.props.activities.activities.map( (activity, i) => <Activity key={i} name={activity.name} address={activity.address} cost={activity.cost} notes={activity.notes} 
+    mondayOpen={activity.monday_open} mondayClose={activity.monday_close} 
+    tuesdayOpen={activity.tuesday_open} tuesdayClose={activity.tuesday_close} 
+    wednesdayOpen={activity.wednesday_open} wednesdayClose={activity.wednesday_close}  
+    thursdayOpen={activity.thursday_open} thursdayClose={activity.thursday_close}  
+    fridayOpen={activity.friday_open} fridayClose={activity.friday_close}  
+    saturdayOpen={activity.saturday_open} saturdayClose={activity.saturday_close}  
+    sundayOpen={activity.sunday_open} sundayClose={activity.sunday_close} />)
+
     return (
       <div>
-        Activity List 
+        <h2>Available Activities:</h2>
+        { activities } 
       </div>
     )
   }
 }
 
-export default ActivityList
+const mapStateToProps = state => {
+  return {
+    activities: state.activities
+  }
+}
+
+export default connect(mapStateToProps)(ActivityList)
