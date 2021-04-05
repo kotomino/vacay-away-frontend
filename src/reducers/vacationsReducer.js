61c52f90a872db52c4b9beb9ad1084d371da8433
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-05 12:32:32 -0400

    loading, set vacation , and add vacation reducer added

--- src/reducers/vacationsReducer.js
+++ src/reducers/vacationsReducer.js
@@ -5,6 +5,22 @@ const initialState = {
 
 const vacationsReducer = (state = initialState, action) => {
   switch(action.type) {
+    case "LOADING":
+      return {
+        ...state,
+        loading: true
+      }
+    case "SET_VACATIONS":
+      return {
+        ...state,
+        loading: false,
+        vacations: action.vacations
+      }
+    case "ADD_VACATION":
+      return {
+        ...state,
+        vacations: [...state.vacations, action.vacation]
+      }
     default:
       return state;
   }
