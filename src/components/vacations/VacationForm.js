5dae799574b1665798b9453171f41dea78b13eb1
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-05 12:31:54 -0400

    create new vacation form

--- src/components/vacations/VacationForm.js
+++ src/components/vacations/VacationForm.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+// import { addVacation } from '.../actions/vacations';
+
 
 class VacationForm extends Component {
+
+  state = {
+    location: "",
+    startDate: "",
+    endDate: "",
+    budget: 0
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.addVacation(this.state, this.props.history);
+  }
+
   render() {
     return (
       <div>
-        Vacation Form
+        <h3>Plan Your Next Vacation</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="location">Location </label>
+            <input type="text" id="title" name="location" value={this.state.location} onChange={this.handleChange} />
+          </div><br/>
+          <div>
+            <label htmlFor="startDate">Start Date </label>
+            <input type="date" id="startDate" name="startDate" value={this.state.startDate} onChange={this.handleChange} />
+          </div><br/>
+          <div>
+            <label htmlFor="endDate">End Date </label>
+            <input type="date" id="endDate" name="endDate" value={this.state.endDate} onChange={this.handleChange} />
+          </div><br/>
+          <div>
+            <label htmlFor="budget">Budget </label>
+            <input type="number" id="budget" name="budget" value={this.state.budget} onChange={this.handleChange} />
+          </div><br/>
+          <input type="submit" value="Create Trip" />
+        </form>
       </div>
     )
   }
