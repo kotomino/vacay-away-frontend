9279444f73bdd39538d57830687d94f371b99f56
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-07 10:37:37 -0400

    added grids and card format

--- src/components/vacations/VacationList.js
+++ src/components/vacations/VacationList.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import Vacation from './Vacation';
-import ActivityList from '../activities/ActivityList';
+import Grid from '@material-ui/core/Grid';
+import { Button, Container } from '@material-ui/core';
+import { deleteVacation } from '../../actions/vacations';
+import Typography from '@material-ui/core/Typography';
+
 
 class VacationList extends Component {
+
+  handleDelete = id => {
+    this.props.deleteVacation(id);
+  }
+
   render() {
+
+    // const date1 = new Date(start_date);
+    // const date2 = new Date(end_date);
+    // const diffTime = Math.abs(date2 - date1);
+    // const diffDays = Math.ceil( 1 + (diffTime / (1000 * 60 * 60 * 24))); 
     
-    const vacations = this.props.vacations.vacations.map( (vacation, i) => <Vacation key={vacation.id} location={ vacation.location } startDate={ vacation.start_date } endDate={ vacation.end_date } budget={vacation.budget} vacation={vacation} />)
-   
+    const vacations = this.props.vacations.map( (vacation, i) => <Vacation key={vacation.id} handleDelete={this.handleDelete} location={ vacation.location } start_date={ vacation.start_date } end_date={ vacation.end_date } budget={vacation.budget} vacation={vacation} />)
+
     return (
-      <div>
-        <h2>Vacation Spots:</h2>
-        { vacations }
-        <ActivityList />
-      </div>
+      <Container>
+        
+        <Grid container spacing={3}>
+          <Grid item xs={12} >
+            <Typography variant="h4" gutterBottom color="primary" align="center">
+              <strong>Planned Trips:</strong>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="right">
+            <Button variant="contained" color="secondary" size="large" href="/vacations/new" >
+              Plan A New Trip
+            </Button>
+          </Grid>
+          { vacations }
+        </Grid><br/>
+          
+      </Container>
     )
   }
 }
 
 const mapStateToProps = state => {
   return {
-    vacations: state.vacations,
-    activities: state.activities
+    vacations: state.vacations.vacations
   }
 }
 
-export default connect(mapStateToProps)(VacationList)
+export default connect(mapStateToProps, { deleteVacation })(VacationList)
