9ce47637de72ab6be4775f81d61c6cbcc65a9726
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-07 10:32:55 -0400

    imported index.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import App from './App';
+import './index.css'
 import activitiesReducer from './reducers/activitiesReducer';
 import vacationsReducer from './reducers/vacationsReducer';
 import reportWebVitals from './reportWebVitals';
