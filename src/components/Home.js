f65c7a7d771ee6fc9f10e864b3ec207ada735d0b
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-07 10:34:01 -0400

    homepage edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import "../stylesheets/main.css"; 
+import Grid from '@material-ui/core/Grid';
+import { Container } from '@material-ui/core';
+
 class Home extends Component {
   render() {
     return (
-      <div className="App">
-          <h2>Welcome to Vacay Away!</h2>
-        </div>
+     <div className="main" >
+       <Container>
+        <Grid container>
+        </Grid>
+       </Container>
+      
+
+    </div>
+
     )
   }
 }
