aaf016fac60516d75177172c8458ce77c6a3cd85
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-07 10:35:42 -0400

    material ui styling added

--- src/components/activities/ActivityList.js
+++ src/components/activities/ActivityList.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import Activity from './Activity';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 
 class ActivityList extends Component {
   
   render() {
-    
-    const activities = this.props.activities.activities.map( (activity, i) => <Activity key={i} name={activity.name} address={activity.address} cost={activity.cost} notes={activity.notes} 
+    const relevantActivities = this.props.activities.filter(activity => activity.vacation.id === this.props.vacation.id)
+   
+    const activities = relevantActivities.map( (activity, i) => <Activity key={i} name={activity.name} address={activity.address} cost={activity.cost} notes={activity.notes} 
     mondayOpen={activity.monday_open} mondayClose={activity.monday_close} 
     tuesdayOpen={activity.tuesday_open} tuesdayClose={activity.tuesday_close} 
     wednesdayOpen={activity.wednesday_open} wednesdayClose={activity.wednesday_close}  
@@ -14,11 +18,15 @@ class ActivityList extends Component {
     fridayOpen={activity.friday_open} fridayClose={activity.friday_close}  
     saturdayOpen={activity.saturday_open} saturdayClose={activity.saturday_close}  
     sundayOpen={activity.sunday_open} sundayClose={activity.sunday_close} />)
-
+    
     return (
       <div>
-        <h2>Available Activities:</h2>
-        { activities } 
+          <Grid item xs={12}>
+            <Typography variant="h5" color="primary" align="center"><strong>Activities:</strong></Typography>
+          </Grid>
+          <Grid container spacing={2}>
+            { activities } 
+          </Grid>
       </div>
     )
   }
@@ -26,7 +34,7 @@ class ActivityList extends Component {
 
 const mapStateToProps = state => {
   return {
-    activities: state.activities
+    activities: state.activities.activities
   }
 }
 
