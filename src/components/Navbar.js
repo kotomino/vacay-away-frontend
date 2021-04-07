b1f2a851086b6fb14b59204ac03c8ad40dbda517
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-07 10:34:58 -0400

    removed new vacation form from nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,6 @@ class Navbar extends Component {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/vacations">Vacations</Link></li>
-          <li><Link to="/vacations/new">Plan a new trip</Link></li>
         </ul>
       </div>
     )
