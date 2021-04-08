5b440728cca68f6299d4f703708d6f3ec593e292
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-08 18:03:48 -0400

    pass in props

--- src/components/vacations/VacationSchedule.js
+++ src/components/vacations/VacationSchedule.js
@@ -6,7 +6,7 @@ import VacationDay from './VacationDay';
 class VacationSchedule extends Component {
 
   render() {
-    const { numOfDays } = this.props;
+    const { numOfDays, activities, vacation, handleUpdate } = this.props;
 
     /* Create daysArray with num of days in trip equaling indices and map over to create <VacationDay> containers */
     const daysArray = []
@@ -15,7 +15,7 @@ class VacationSchedule extends Component {
        daysArray.push(`Day ${i + 1}`);
     }
    
-    const days = daysArray.map((day, i) => <VacationDay key={i} day={day} />)
+    const days = daysArray.map((day, i) => <VacationDay key={i} numOfDays={numOfDays} day={day} activities={activities} vacation={vacation} handleUpdate={handleUpdate} />)
 
     return (
       <Grid container spacing={2} className="slider">
