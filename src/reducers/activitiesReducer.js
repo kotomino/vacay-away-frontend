0c5bfe9d17bc4d511f3a3c5ea8c64e76d42fc57a
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-08 18:05:39 -0400

    reducer for add, update, and delete activity

--- src/reducers/activitiesReducer.js
+++ src/reducers/activitiesReducer.js
@@ -16,6 +16,27 @@ const activitiesReducer = (state = initialState, action) => {
         loading: false,
         activities: action.activities
       }
+    case "ADD_ACTIVITY":
+      console.log('ADD_ACTVIVITY reducer:', action)
+    return {
+      ...state,
+      activities: [...state.activities, action.activity]
+    }
+    case "UPDATE_ACTIVITY_DAY":
+      console.log('UPDATE_ACTVIVITY reducer:', action)
+   
+      return {
+        ...state,
+        activities: [...state.activities, action.activity]
+      }
+    case "DELETE_ACTIVITY":
+      console.log('DELETE_ACTVIVITY reducer:', action)
+      const newActivities = state.activities.filter(activity => activity.id !== action.id)
+      return {
+        ...state,
+        activities: newActivities
+      }
+
     default:
       return state;
   }
