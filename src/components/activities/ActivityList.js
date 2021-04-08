3c474b1f9055acdaacf45673df9437d732364633
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-08 18:02:57 -0400

    handle delete activity

--- src/components/activities/ActivityList.js
+++ src/components/activities/ActivityList.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import Activity from './Activity';
+import { deleteActivity } from '../../actions/activities';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 
 class ActivityList extends Component {
+
+  handleDelete = id => {
+    this.props.deleteActivity(id)
+  }
   
   render() {
-    const relevantActivities = this.props.activities.filter(activity => activity.vacation.id === this.props.vacation.id)
+    const relevantActivities = this.props.activities.filter(activity => activity.vacation.id === this.props.vacation.id && activity.day === "Undecided")
    
-    const activities = relevantActivities.map( (activity, i) => <Activity key={i} name={activity.name} address={activity.address} cost={activity.cost} notes={activity.notes} 
+    const activities = relevantActivities.map( (activity, i) => <Activity key={i} handleUpdate={this.props.handleUpdate} numOfDays={this.props.numOfDays} activity={activity} handleDelete={this.handleDelete} name={activity.name} address={activity.address} cost={activity.cost} notes={activity.notes} 
     mondayOpen={activity.monday_open} mondayClose={activity.monday_close} 
     tuesdayOpen={activity.tuesday_open} tuesdayClose={activity.tuesday_close} 
     wednesdayOpen={activity.wednesday_open} wednesdayClose={activity.wednesday_close}  
@@ -22,7 +27,7 @@ class ActivityList extends Component {
     return (
       <div>
           <Grid item xs={12}>
-            <Typography variant="h5" color="primary" align="center"><strong>Activities:</strong></Typography>
+            <Typography variant="h5" color="primary" align="center"><strong>Activities Without Day:</strong></Typography>
           </Grid>
           <Grid container spacing={2}>
             { activities } 
@@ -38,4 +43,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(ActivityList)
+export default connect(mapStateToProps, { deleteActivity })(ActivityList)
