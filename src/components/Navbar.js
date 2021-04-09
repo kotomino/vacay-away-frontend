4fa16a4c2d3e21ec31f4ca5fc4147cbb3cb41c89
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-09 00:50:18 -0400

    styled nav with material ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,53 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import CardTravelIcon from '@material-ui/icons/CardTravel';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import LoyaltyTwoToneIcon from '@material-ui/icons/LoyaltyTwoTone';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/vacations">Vacations</Link></li>
-        </ul>
-      </div>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  navbar: {
+    backgroundColor: fade("#F0FFFF", 0),
   }
+}));
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.navbar} elevation={0} >
+        <Toolbar>
+          <LoyaltyTwoToneIcon color="primary"/>
+          <Typography variant="h5" color="primary" className={classes.title}>
+            Vacay Away
+          </Typography>
+          <IconButton href="/" >
+            <HomeRoundedIcon color="primary" />
+          </IconButton>
+          <IconButton href="/vacations" >
+            <CardTravelIcon color="primary" />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 }
 
 export default Navbar
+
+
+
+
