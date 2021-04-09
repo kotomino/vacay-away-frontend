b5943e8072e6ae37506fc2a2655c3923400b6f93
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-09 00:51:23 -0400

    content updated

--- src/components/activities/ActivityWithDay.js
+++ src/components/activities/ActivityWithDay.js
@@ -2,32 +2,45 @@ import React from 'react'
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import { fade } from '@material-ui/core/styles/colorManipulator';
-import { Typography } from '@material-ui/core';
+import { Container, Grid, Typography } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
 import DaySelector from './DaySelector';
 
 const useStyles = makeStyles({
   activity: {
-    backgroundColor: fade("#F0FFFF", 0.4),
-    height: 150
+    backgroundColor: fade("#22293D", 0.7),
+    
   },
+  flexChild: {
+    width: 60,
+    height: 70,
+    margin: 10
+  }
 });
 
-function ActivityWithDay({ activity, numOfDays, handleUpdate }) {
+function ActivityWithDay({ activity, numOfDays, handleUpdate, day }) {
   console.log(activity)
   const classes = useStyles();
 
+
   return (
-    <Card container className={classes.activity} >
-      <CardContent>
-        <Typography>
-          { activity.name }< br/>
-          ${ activity.cost }
-        </Typography>
-        <DaySelector numOfDays={numOfDays} activity={activity} handleUpdate={handleUpdate}/>
-      </CardContent>
-    </Card>
-      
+    <Grid item xs={12}>
+      <Card container className={[classes.activity, "flex-container"].join(" ")} >
+        
+          <div className={[classes.flexChild, "flex-child"].join(" ")} >
+          <Typography variant="h5" color="textSecondary">
+            { activity.name }
+          </Typography>
+          <Typography variant="h6" color="textSecondary">
+            ${ activity.cost }
+          </Typography>
+          </div>
+          <div className={[classes.flexChild, "flex-child"].join(" ")}>
+          <DaySelector numOfDays={numOfDays} activity={activity} handleUpdate={handleUpdate} day={day} />
+          </div>
+        
+      </Card>
+    </Grid>  
   )
 }
 
