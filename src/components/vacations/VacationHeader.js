d45b2451f4aee6fb2291a01c10441e0d8f981ffe
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-10 01:10:06 -0400

    removed grid and cardheader imports

--- src/components/vacations/VacationHeader.js
+++ src/components/vacations/VacationHeader.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { fade } from '@material-ui/core/styles/colorManipulator';
