9393b1969d2d00fa21c13c854dff0c8e0c05abba
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-11 13:11:41 -0400

    created loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+
+function Loading() {
+  return (
+    <Typography variant="h4" color="textSecondary" align="center">
+      Loading...
+    </Typography>
+  )
+}
+
+export default Loading
