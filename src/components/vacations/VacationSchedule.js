b7ba7d0f8dd005564412d77aeeea3abcb7262769
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-11 13:17:11 -0400

    slider fixed

--- src/components/vacations/VacationSchedule.js
+++ src/components/vacations/VacationSchedule.js
@@ -3,26 +3,22 @@ import Grid from '@material-ui/core/Grid';
 import VacationDay from './VacationDay';
 
 
-class VacationSchedule extends Component {
+function VacationSchedule ({ numOfDays, activities, vacation, handleUpdate }) {
 
-  render() {
-    const { numOfDays, activities, vacation, handleUpdate } = this.props;
+  /* Create daysArray with num of days in trip equaling indices and map over to create <VacationDay> containers */
+  const daysArray = []
 
-    /* Create daysArray with num of days in trip equaling indices and map over to create <VacationDay> containers */
-    const daysArray = []
-
-    for (let i = 0; i < numOfDays; i++) {
-       daysArray.push(`Day ${i + 1}`);
-    }
-   
-    const days = daysArray.map((day, i) => <VacationDay key={i} numOfDays={numOfDays} day={day} activities={activities} vacation={vacation} handleUpdate={handleUpdate} />)
-
-    return (
-      <Grid container spacing={2} className="slider">
-        { days }
-      </Grid>
-    )
+  for (let i = 0; i < numOfDays; i++) {
+      daysArray.push(`Day ${i + 1}`);
   }
+  
+  const days = daysArray.map((day, i) => <VacationDay key={i}  numOfDays={numOfDays} day={day} activities={activities} vacation={vacation} handleUpdate={handleUpdate} />)
+
+  return ( 
+    <Grid container wrap='nowrap' spacing={2} className="vacationSlider" >
+      { days }
+    </Grid>
+  )
 }
 
 export default VacationSchedule
