4d34dae64426e87b139aac304a6c100a0d56d618
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-11 13:14:09 -0400

    styling

--- src/components/vacations/VacationDay.js
+++ src/components/vacations/VacationDay.js
@@ -11,6 +11,7 @@ const useStyles = makeStyles({
   dayPlanner: {
     backgroundColor: fade("#F0FFFF", 0.4),
     height: 340,
+    width: 340
   },
 });
 
@@ -18,18 +19,16 @@ function VacationDay({ day, activities, vacation, numOfDays, handleUpdate }) {
   
   const activitiesForDay = activities.filter(activity => activity.day === day && activity.vacation.id === vacation.id )
 
-  console.log(day, activitiesForDay)
-
   const displayActivities = activitiesForDay.map(activity => <ActivityWithDay day={ day } numOfDays={numOfDays} activity={activity} vacation={vacation} handleUpdate={handleUpdate} />);
 
   const classes = useStyles();
   
     return (
-      <Grid item xs={4} sm={4} spacing={1}>
-        <Typography variant="h5" color="textPrimary"><center><strong>{ day }</strong></center></Typography>
-        <Card container className={[classes.dayPlanner, "slide"].join(" ")} elevation={5}>
-          <CardContent >
-            <Grid container spacing={1}>
+      <Grid item spacing={1} >
+        <Typography variant="h4" color="textPrimary"><center><strong>{ day }</strong></center></Typography>
+        <Card container className={[classes.dayPlanner, "flex-section"].join(" ")} elevation={5} >
+          <CardContent className={"flex-col-scroll"}>
+            <Grid container spacing={1} >
             { displayActivities }
             </Grid>
           </CardContent>
